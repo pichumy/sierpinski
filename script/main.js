2127aeb830a6528c2e3a5f1d2e8b453f11808f68
thegecko <github@thegecko.org>
2014-06-18 18:34:12 +0000

    Added pure.css and tidied the code and layout

--- script/main.js
+++ script/main.js
@@ -1,14 +1,27 @@
 Zepto(function($) {
-   
+
     // These guys change the amount we move by
     var panStep = 30;
-    var scaleStep = 1.03;
-    
+    var scaleStep = 1.1;
+
     var context = {};
-    var triangle = {};
     var midPoint = {};
-    
-    // Parse an rgb(x, x, x) colour into #xxxxxx
+    var triangles = [];
+
+    // Extend the triangle array instance with a function to act on all items
+    triangles.each = function() {
+        // Turn the passed arguments list into a proper array
+        var args = [].slice.call(arguments);
+
+        // Grab the function to call, arguments will be everything else
+        var fn = args.shift();
+
+        $.each(this, function(i, triangle){
+            triangle[fn].apply(this, args);
+        });
+    };
+
+    // Parse an rgb(x, x, x) colour into hex #xxxxxx
     function rgb2hex(rgb) {
         rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
         function hex(num) {
@@ -16,21 +29,27 @@ Zepto(function($) {
         }
         return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
     }
-    
+
     function init() {
         // Ratio of side to height of an equilateral triangle
         var ratio = Math.sqrt(3) / 2;
-        var explorer = $("#explorer");
-        var colour = rgb2hex(explorer.css('color'));
+
+        // Full screen
         var stageWidth = window.innerWidth;
         var stageHeight = window.innerHeight;
 
+        // Get the css colour for the main triangle
+        var explorer = $("#explorer");
+        var colour = rgb2hex(explorer.css('color'));
+
+        // Setup the canvas
         var canvas = explorer[0];
         canvas.width = stageWidth;
         canvas.height = stageHeight;
         context = canvas.getContext("2d");
+        midPoint = { x: stageWidth >> 1, y: stageHeight >> 1 };
 
-        var width, height, x, y;
+        var x, y, width, height;
 
         // Determine biggest equilateral triangle to fit on screen
         if (ratio * stageWidth < stageHeight) {
@@ -43,93 +62,95 @@ Zepto(function($) {
             // Triangle takes up entire height
             width = stageHeight / ratio;
             height = stageHeight;
-            x = stageWidth / 2 - width / 2;
+            x = (stageWidth - width) >> 1;
             y = 0;           
         }
 
-        // Our initial triangle points
-        var p1 = { x: x, y: stageHeight };
-        var p2 = { x: stageWidth / 2, y: y };
-        var p3 = { x: x + width, y: stageHeight };
+        // Empty the triangle array
+        triangles.length = 0;
 
-        triangle = new sierpinski(p1, p2, p3, { colour: colour });
-        midPoint = { x: canvas.width / 2, y: canvas.height / 2 };
+        // Add our main triangle
+        triangles.push(new sierpinski(x, y, width, height, { colour: colour }));
+
+        // Add another smaller one, just for fun
+        triangles.push(new sierpinski(x, y, width >> 2, height >> 2));
 
         render();
     }
-        
+
     function render() {
         context.clearRect (0, 0, context.canvas.width, context.canvas.height);
-        triangle.render(context);
+        triangles.each("render", context);
     }
-    
+
     // User interactions
     $(window).resize(function() {
         init();
     });
 
+    $(window).on("mousewheel DOMMouseScroll", function(e) {
+        var origin = { x: e.clientX, y: e.clientY };
+        var factor = (e.wheelDelta || -e.detail) > 0 ? scaleStep : 1 / scaleStep;
+
+        triangles.each("scale", origin, factor);
+        render();
+    });
+
+    $(window).on("mousedown", function(e) {
+        // Store origin so we have a point to calculate offset
+        var origin = { x: e.clientX, y: e.clientY };
+
+        function move(e) {
+            var offset = { x: e.clientX - origin.x, y: e.clientY - origin.y };
+            triangles.each("move", offset);
+            render();
+
+            // Update origin, otherwise the move becomes exponential :)
+            origin = { x: e.clientX, y: e.clientY };
+        }
+
+        $(window).on("mousemove", move);
+
+        // Remove mousemove event handler when mouse is released
+        $(window).on("mouseup", function() {
+            $(window).off("mousemove", move);
+        });
+    });
+
     $("#left").on("click", function() {
-        triangle.move({ x: panStep, y: 0 });
+        triangles.each("move", { x: panStep, y: 0 });
         render();
     });
-    
+
     $("#right").on("click", function() {
-        triangle.move({ x: -panStep, y: 0 });
+        triangles.each("move", { x: -panStep, y: 0 });
         render();
     });
-    
+
     $("#up").on("click", function() {
-        triangle.move({ x: 0, y: panStep });
+        triangles.each("move", { x: 0, y: panStep });
         render();
     });
-    
+
     $("#down").on("click", function() {
-        triangle.move({ x: 0, y: -panStep });
+        triangles.each("move", { x: 0, y: -panStep });
         render();
     });
-    
+
     $("#in").on("click", function() {
-        triangle.scale(midPoint, scaleStep);
+        triangles.each("scale", midPoint, scaleStep);
         render();
     });
-    
+
     $("#out").on("click", function() {
-        triangle.scale(midPoint, 1 / scaleStep);
+        triangles.each("scale", midPoint, 1 / scaleStep);
         render();
     });
-    
+
     $("#reset").on("click", function() {
         init();
     });
-    
-    $("#explorer").on("mousewheel DOMMouseScroll", function(e) {
-        var origin = { x: e.layerX, y: e.layerY };
-        var factor = (e.wheelDelta || -e.detail) > 0 ? scaleStep : 1 / scaleStep;
-        triangle.scale(origin, factor);
-        render();	
-    });
-    
-    $("#explorer").on("mousedown", function(e) {
-        // Store origin so we have a point to calculate offset
-        var origin = { x: e.layerX, y: e.layerY };
-        
-        function move(e) {
-            var offset = { x: e.layerX - origin.x, y: e.layerY - origin.y };
-            triangle.move(offset);
-            render();
-            
-            // Update origin, otherwise the move becomes exponential :)
-            origin = { x: e.layerX, y: e.layerY };
-        }
-        
-        $("#explorer").on("mousemove", move);
-        
-        // Remove mousemove event handler when mouse is released
-        $("#explorer").on("mouseup", function() {
-	        $("#explorer").off("mousemove", move);        
-        });
-    });        
 
     // Let's go!
     init();
-})
+});
--- script/sierpinski.js
+++ script/sierpinski.js
@@ -1,31 +1,31 @@
-// Sierpinski triangle renderer
+// Sierpinski triangle object
 // Rob Moran 17/06/2014
 
-// The triangle points work like this
-
-        //\\
-       //p2\\ 
-      //    \\  
-     //______\\
-    //\n1  n2/\\
-   //  \    /  \\
-  //    \n3/    \\
- //p1    \/    p3\\
-//================\\
-
+// The triangle points work like this:
+//
+//            /\
+//           /p2\
+//          /    \
+//         /______\
+//        /\n1  n2/\
+//       /  \    /  \
+//      /    \n3/    \
+//     /p1____\/____p3\
+  
 // Main 'constructor'
-sierpinski = function(p1, p2, p3, opts) {
-   
-    this.p1 = p1;
-    this.p2 = p2;
-    this.p3 = p3;
-    
+sierpinski = function(x, y, width, height, opts) {
+
+    // Our initial triangle points
+    this.p1 = { x: x, y: y + height };
+    this.p2 = { x: x + (width >> 1), y: y };
+    this.p3 = { x: x + width, y: y + height };
+
     // Default options
     this.options = {
-        colour: "#0000FF",
-        minimumSize: 5
+        colour: "#f00",
+        quality: 5 // Lower is better/slower
     };
-    
+
     // Merge options
     if (opts) {
         for (var item in opts) {
@@ -38,22 +38,22 @@ sierpinski = function(p1, p2, p3, opts) {
 
 // Function to translate the triangle points for panning
 sierpinski.prototype.move = function(offset) {
-    
+
     function translate(point) {
         return {
             x: point.x + offset.x,
             y: point.y + offset.y
       	};
     }
-    
+
     this.p1 = translate(this.p1);
     this.p2 = translate(this.p2);
     this.p3 = translate(this.p3);
-}
+};
 
 // Function to translate the triangle points for zooming
 sierpinski.prototype.scale = function(origin, factor) {
-    
+
     // Shape dilation
     function translate(point) {
         return {
@@ -61,18 +61,18 @@ sierpinski.prototype.scale = function(origin, factor) {
             y: ((point.y - origin.y) * factor) + origin.y
         };
     }
-    
+
     this.p1 = translate(this.p1);
     this.p2 = translate(this.p2);
     this.p3 = translate(this.p3);
-}
+};
 
 // The main render function
 sierpinski.prototype.render = function(context) {
-    
+
     // Some context variables
     var canvas = context.canvas;
-    var minimumSize = this.options.minimumSize;
+    var quality = this.options.quality;
 
     // Builds a triangle object from 3 co-ordinates and includes the 3 mid points
     function createTriangle(p1, p2, p3) {
@@ -97,44 +97,46 @@ sierpinski.prototype.render = function(context) {
             width: Math.abs(p3.x - p1.x),
             height: Math.abs(p1.y - p2.y)
         };
-    } 
-       
-    // Draw the outer triangle by plotting the path counter-clockwise
-    function draw(context, triangle) {   
-        context.moveTo(triangle.p1.x, triangle.p1.y);
-        context.lineTo(triangle.p2.x, triangle.p2.y);
-        context.lineTo(triangle.p3.x, triangle.p3.y);
-        context.lineTo(triangle.p1.x, triangle.p1.y);
     }
-    
-    // Remove the inner triangles by plotting the path clockwise
-    function remove(context, triangle) {
-    	context.moveTo(triangle.n1.x, triangle.n1.y);
-    	context.lineTo(triangle.n3.x, triangle.n3.y);
-    	context.lineTo(triangle.n2.x, triangle.n2.y);
-    	context.lineTo(triangle.n1.x, triangle.n1.y);
+
+    // Function to determine whether a triangle is visible
+    function visible(triangle, bounds) {
+        if (triangle.x > bounds.width
+            || triangle.y > bounds.height
+            || triangle.x + triangle.width < 0
+            || triangle.y + triangle.height < 0)
+        {
+            return false;
+        }
+
+        return true;
     }
-    
+
+    // Plot a triangle path
+    function plot(context, p1, p2, p3) {
+        context.moveTo(p1.x, p1.y);
+        context.lineTo(p2.x, p2.y);
+        context.lineTo(p3.x, p3.y);
+        context.lineTo(p1.x, p1.y);
+    }
+
+    // Recursion
     function recurse(context, triangle) {
 
         // Stop if the triangle is too small
-        if (triangle.width < minimumSize) {
+        if (triangle.width < quality) {
             return;
         }
 
         // Stop if the triangle is off screen
-        if (triangle.x > canvas.width
-            || triangle.y > canvas.height
-            || triangle.x + triangle.width < 0
-            || triangle.y + triangle.height < 0)
-        {
+        if (!visible(triangle, canvas)) {
             return;
         }
 
-        // Remove inner triangle
-        remove(context, triangle);
+        // Remove inner triangle by plotting the path counter-clockwise
+        plot(context, triangle.n1, triangle.n3, triangle.n2);
 
-        // Create 3 smaller triangles from larger one
+        // Create 3 smaller triangles from the larger one
         var t1 = createTriangle(triangle.p1, triangle.n1, triangle.n3);
         var t2 = createTriangle(triangle.n1, triangle.p2, triangle.n2);
         var t3 = createTriangle(triangle.n3, triangle.n2, triangle.p3);
@@ -148,19 +150,16 @@ sierpinski.prototype.render = function(context) {
     // Create initial triangle object
     var triangle = createTriangle(this.p1, this.p2, this.p3);
 
-    // Start the path
+    // Start the path and colour the main triangle
     context.beginPath();
     context.fillStyle = this.options.colour;
 
-    // Draw the outer triangle
-    draw(context, triangle);
+    // Draw the outer triangle by plotting the path clockwise
+    plot(context, triangle.p1, triangle.p2, triangle.p3);
 
     // Begin recursion
     recurse(context, triangle);
 
     // Complete the path
     context.fill();
-}
-        
-
-        
+};
